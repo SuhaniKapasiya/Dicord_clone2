f85303c22ab6e6854038ee4b09f45b3451167783
SuhaniKapasiya <107610361+SuhaniKapasiya@users.noreply.github.com>
2023-06-26 20:18:27 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'whitney': ['Whitney', 'sans-serif'],
+      'ginto': ['Ginto', 'sans-serif'],
+      'ggSans': ['ggSans', 'sans-serif'],
+    },
+  },
+  plugins: [],
+};
+
